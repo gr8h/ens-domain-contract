c643a6e4221916057e88fa65d3616af169228e25
gr8h <hesham.shabana@hotmail.com>
2022-03-18 18:54:48 +0100

    Init

---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy();
+    await domainContract.deployed();
+    console.log("Contract deployed to:", domainContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
