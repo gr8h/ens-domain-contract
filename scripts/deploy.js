644694a5d1df40cff00c93c5c1b430485508d88b
gr8h <hesham.shabana@hotmail.com>
2022-03-19 14:26:09 +0100

    finalize the contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,26 @@
+
 const main = async () => {
-    const [deployer] = await hre.ethers.getSigners();
-    const accountBalance = await deployer.getBalance();
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy("gr8");
+    await domainContract.deployed();
   
-    console.log("Deploying contracts with account: ", deployer.address);
-    console.log("Account balance: ", accountBalance.toString());
+    console.log("Contract deployed to:", domainContract.address);
   
-    const waveContractFactory = await hre.ethers.getContractFactory("Domains");
-    const waveContract = await waveContractFactory.deploy({
-        value: hre.ethers.utils.parseEther("0.001"),
-      });
-    await waveContract.deployed();
+    // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of gr8s lol
+    let txn = await domainContract.register("gr8",  {value: hre.ethers.utils.parseEther('0.1')});
+    await txn.wait();
+    console.log("Minted domain banana.gr8");
   
-    console.log("WavePortal address: ", waveContract.address);
-  };
+    txn = await domainContract.setRecord("gr8", "Am I gr8 or a ninja??");
+    await txn.wait();
+    console.log("Set record for banana.gr8");
+  
+    const address = await domainContract.getAddress("gr8");
+    console.log("Owner of domain gr8:", address);
+  
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+  }
   
   const runMain = async () => {
     try {
