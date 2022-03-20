aa89a001363c19519d0fb0f433f71b1f2629d139
gr8h <hesham.shabana@hotmail.com>
2022-03-20 21:00:17 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Buildspace Project - Cool Domains
+
+This project is about making your own domain service — where people can buy their own domain from your smart contract and own it.
+
+This is the SmartContract
+
+URL: https://domain-portal-9w8trngf3-gr8h.vercel.app/
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,13 @@ require("dotenv").config();
   solidity: "0.8.10",
   networks: {
 		mumbai: {
-      url: process.env.ALCHEMY_MUMBAI_URL,
-      accounts: [process.env.PRIVATE_KEY],
-		}
+         url: process.env.ALCHEMY_MUMBAI_URL,
+         accounts: [process.env.PRIVATE_KEY],
+		},
+      mainnet: {
+         chainId: 1,
+         url: process.env.PROD_ALCHEMY_KEY,
+         accounts: [process.env.PRIVATE_KEY],
+       },
   }
 };
