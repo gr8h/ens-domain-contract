f29f2a7c65130961c8857f6213b3fa65140d6975
gr8h <hesham.shabana@hotmail.com>
2022-03-20 16:28:37 +0100

    withdraw money from the contract

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -16,6 +16,7 @@ contract Domains is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     string public tld;
+    address payable public owner;
 
     // We'll be storing our NFT images on chain as SVGs
     string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><svg x="85" y="-60" width="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1333.3 1062.26" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"><defs><style>.fil0{fill:#2058b6}</style></defs><g id="Layer_x0020_1"><g id="_1473327709744"><path stroke-width="10" class="fil0" d="M512.04 551.2c-39.86 7.98-39.86 55.8-31.89 119.6 7.98 71.75 31.89 119.6 71.76 111.63 39.87 0 39.87-55.82 31.9-119.6-7.97-63.78-23.93-111.63-71.76-111.63z"/><path stroke-width="10" class="fil0" d="M1333.3 630.93s-23.94-111.61-143.53-207.29c-111.63-79.74-231.23-71.77-231.23-71.77s31.9-63.79 119.6-111.63c79.74-39.87 191.35-47.82 191.35-47.82s-71.75-87.72-215.27-143.53C878.8-14.9 735.29-14.9 559.87 40.92 368.5 104.71 232.96 256.2 232.96 256.2S41.6 200.38 9.7 248.21c-39.86 47.86 55.82 231.24 55.82 231.24S41.59 535.26 49.56 638.9c7.98 111.63 71.78 191.38 71.78 191.38-31.9 7.95-63.79 23.9-63.79 47.82 0 23.93 47.84 47.85 103.65 47.85h23.93c55.8 55.8 135.54 127.55 255.14 135.53 151.49 7.97 247.17-47.82 310.96-63.78 71.75-23.93 175.41-23.93 239.19-23.93 191.36 23.93 271.08 71.78 271.08 71.78s7.97-87.73-71.75-207.33c-63.79-87.7-191.37-119.58-191.37-119.58s63.79-55.82 143.53-71.78c103.67-23.89 191.38-15.94 191.38-15.94zM121.36 766.48c-15.95-31.9-23.93-63.78-31.9-159.48 0-47.82 15.95-103.63 39.86-111.61 31.9-7.97 31.9 47.85 47.85 95.69 0 15.92 15.94 39.85 23.91 63.78-7.97-7.98-7.97-7.98-7.97 0-23.91 0-31.89 47.85-15.94 103.65 7.97 31.9 23.91 63.8 39.86 71.78-15.95 0-31.89-7.97-55.81-7.97 0 0-7.97 0-7.97 7.97-7.98-15.98-23.93-39.88-31.89-63.81zm494.33 0c-39.86 79.73-103.65 103.65-175.41 103.65-71.78 0-119.6-39.85-191.37-31.9 15.96-15.93 15.96-55.8 7.97-103.65-7.97-7.97-7.97-15.93-15.95-31.87 23.94 15.95 39.87-7.97 39.87-7.97s7.96-119.6 47.83-199.34c31.89-63.78 79.73-111.63 151.49-119.59 63.79-15.95 111.63 31.89 135.54 103.65 23.93 71.74 31.91 207.3.01 287.02z"/></g></g></svg><defs><linearGradient id="B" x1="0" y1="0" x2="27" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset="1" stop-color="#093871" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
@@ -26,6 +27,7 @@ contract Domains is ERC721URIStorage {
     mapping(string => string) public emails;
 
     constructor(string memory _tld) payable ERC721("Sonic Name Service", "BNS"){
+        owner = payable(msg.sender);
         tld = _tld;
         console.log("%s is the Name service Domain.", _tld);
     }
@@ -105,4 +107,20 @@ contract Domains is ERC721URIStorage {
     function getRecord(string calldata name) public view returns(string memory) {
         return records[name];
     }
+
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+
+    function withdraw() public onlyOwner {
+        uint amount = address(this).balance;
+        
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to withdraw Matic");
+    } 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -10,19 +10,38 @@ const main = async () => {
     console.log("Contract deployed by:", owner.address);
 
     // the second variable - value. This is the money
-    const txn = await domainContract.register("Tails",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("Tails",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
     const domainOwner = await domainContract.getAddress("Tails");
     console.log("Owner of domain Tails:", domainOwner);
-
+    
     // Trying to set a record that doesn't belong to me!
     // txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
 
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
+    // Quick! Grab the funds from the contract! (as superCoder)
+    try {
+      txn = await domainContract.connect(randomPerson).withdraw();
+      await txn.wait();
+    } catch(error){
+      console.log("Could not rob contract");
+    }
+
+    // Let's look in their wallet so we can compare later
+    let ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+    console.log("Balance of owner before withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+
+    txn = await domainContract.connect(owner).withdraw();
     await txn.wait();
+
+    const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+    ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+    console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+    console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
   };
   
   const runMain = async () => {
